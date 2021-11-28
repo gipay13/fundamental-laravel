eb1893d68e6b2674a77ef10358c124a9b5d6b615
gipay13 <ghifariansar13@gmail.com>
2021-11-28 11:37:45 +0700

    pre install oauth laravel/breeze

--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('welcome');
 })->name('home');
 
-Route::get('signin', function () {
-    return view('signin');
-})->name('signin');
+Route::get('login', function () {
+    return view('login');
+})->name('login');
 
 Route::get('checkout', function () {
     return view('checkout');
